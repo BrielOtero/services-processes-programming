13fb60f8ead7a8b02a89615384b0c90cb7e7a58d
SrMazas <info.otero.gabriel@gmail.com>
2022-09-30 14:02:46 +0200

    Save

---
+++ 01-multithreading/05-exercise/05-exercise/Horse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_exercise
+{
+    internal class Horse
+    {
+        public int position;
+
+        public void Run()
+        {
+            Random r = new Random();
+            position += r.Next(1, 3);
+            Thread.Sleep(r.Next(1, 3) * 1000);
+        }
+    }
+
+}
--- 01-multithreading/05-exercise/05-exercise/Program.cs
+++ 01-multithreading/05-exercise/05-exercise/Program.cs
@@ -2,9 +2,119 @@
 {
     internal class Program
     {
+        static private void paintSubmenu(string[] options, int option, string title, bool showIndex)
+        {
+
+            Console.Clear();
+            Console.WriteLine($"\n{title,-30}\n\n");
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == option)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                if (showIndex)
+                {
+                    Console.WriteLine($"{i}. {options[i],-30}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{options[i],-30}");
+                }
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+            }
+        }
+
+        static private int subMenu(string title, bool showIndex, params string[] options)
+        {
+            bool exit = false;
+
+            int option = 0;
+            int select = -1;
+
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            paintSubmenu(options, option, title, showIndex);
+            Console.WriteLine($"{"Pulse ENTER to select",-30}");
+
+            do
+            {
+                ConsoleKeyInfo tecla = Console.ReadKey();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        option = option < options.Length - 1 ? option + 1 : option;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        option = option > 0 ? option - 1 : option;
+                        break;
+                    case ConsoleKey.Enter:
+
+                        select = option;
+                        exit = true;
+                        break;
+                }
+                paintSubmenu(options, option, title, showIndex);
+                Console.WriteLine($"{"Pulse ENTER to select",-30}");
+
+            } while (!exit);
+
+            Console.Clear();
+
+            return select;
+        }
+        private static readonly object l = new object();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int menu;
+            //menu = subMenu("Choose your WINNER", true, new string[] { "Perdigon", "Pegaso", "Troya", "Rocinante", "Spirit" });
+            Thread[] threads = new Thread[5];
+
+
+            new Thread(() =>
+            {
+                Horse h = new Horse();
+                while (h.position < 15)
+                {
+
+                    h.Run();
+                    Console.SetCursorPosition(h.position, 0);
+                    Console.Write("*");
+                }
+                lock (l)
+                {
+                    Monitor.Pulse(l);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                Horse h = new Horse();
+                while (h.position < 15)
+                {
+
+                    h.Run();
+                    Console.SetCursorPosition(h.position, 1);
+                    Console.Write("*");
+                }
+                lock (l)
+                {
+                    Monitor.Pulse(l);
+                }
+            }).Start();
+
+            lock (l)
+            {
+                Monitor.Wait(l);
+            }
+
+
+
         }
     }
 }
