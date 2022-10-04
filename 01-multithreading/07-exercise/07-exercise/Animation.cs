d54135e9c92ce5a21df2ec58c18be0dd4ae0b76e
Gabriel O <info.otero.gabriel@gmail.com>
2022-10-04 08:03:35 +0200

    Save

---
+++ 01-multithreading/07-exercise/07-exercise/Animation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_exercise
+{
+    internal class Animation
+    {
+        private static readonly object l = new object();
+        private bool finish = false;
+        private int cont = 0;
+        private int win = 20;
+        public void Start()
+        {
+            Thread player1 = new Thread(runtime);
+            Thread player2 = new Thread(runtime);
+            Thread display = new Thread(animationRuntime);
+
+
+            //player1.Start(0);
+            //player2.Start(1);
+            display.Start(3);
+
+        }
+        private void animationRuntime(Object index)
+        {
+            int cont = -1;
+            string[] animation = { "|", "/", "-", "\\" };
+
+
+            while (!finish)
+            {
+                if (cont < 3)
+                {
+                    cont++;
+                }
+                else
+                {
+                    cont = 0;
+                }
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(0, (int)index);
+                Console.Write(animation[cont]);
+                Thread.Sleep(200);
+            }
+        }
+        private void runtime(object index)
+        {
+            int rand;
+            int randTime;
+            Random r = new Random();
+
+            lock (l)
+            {
+                while (!finish)
+                {
+                    rand = r.Next(1, 11);
+                    randTime = r.Next(100, 100 * rand);
+                    Console.CursorVisible = false;
+                    Console.SetCursorPosition(0, (int)index);
+                    Console.Write(rand);
+                    Thread.Sleep(randTime);
+                }
+            }
+
+        }
+    }
+}
--- 01-multithreading/07-exercise/07-exercise/Program.cs
+++ 01-multithreading/07-exercise/07-exercise/Program.cs
@@ -2,32 +2,13 @@
 {
     internal class Program
     {
-        private delegate void Execution();
-        private bool finish = false;
         static void Main(string[] args)
         {
-            Thread player1= new Thread(runtime);
-            Thread player2;
-            Thread display;
-
+            Animation a = new Animation();
+            a.Start();
         }
 
-        private void runtime()
-        {
-            int rand;
-            int randTime;
-            Random r = new Random();
-            while (!finish)
-            {
-
-                rand = r.Next(1, 11);
-                randTime = r.Next(100, 100 * rand);
-                
-                Console.WriteLine(rand);
-
-                Thread.Sleep(randTime);
-            }
-        }
+    
 
 
     }
