c840b974d98fc7562f84c8c46855861ecb0648d5
SrMazas <info.otero.gabriel@gmail.com>
2023-01-23 16:49:41 +0100

    save

---
+++ 03-networking/01-exercise/01-exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace _01_exercise
+{
+            internal class Program
+            {
+                        static void Main(string[] args)
+                        {
+                                    
+
+                                    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                                    int port = 135;
+                                    IPEndPoint
+                        }
+            }
+}
