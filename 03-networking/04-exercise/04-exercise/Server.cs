c8be0ad95a69a74c88408d2e9fe587b7a447ec8b
Gabriel O <info.otero.gabriel@gmail.com>
2023-02-02 21:43:37 +0100

    save

--- 03-networking/04-exercise/04-exercise/Server.cs
+++ 03-networking/04-exercise/04-exercise/Server.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace _04_exercise
 {
@@ -18,13 +20,12 @@ namespace _04_exercise
         private const int SECONDARY_PORT = 1024;
         private readonly string PATH_NAMES = Path.Combine(Environment.GetEnvironmentVariable("userprofile"), "usuarios.txt");
 
-
         private Socket serverSocket;
         private Socket clientSocket;
 
-        private struct User
+        private enum eCommands
         {
-
+            LIST, ADD, DEL, CHPIN, EXIT, SHUTDOWN
         }
 
 
@@ -46,19 +47,25 @@ namespace _04_exercise
             users = fileUsers;
         }
 
-        private void ReadPin(string path)
+        private int ReadPin(string path)
         {
             int response;
 
             using (FileStream fs = new(path, FileMode.Open))
             using (BinaryReader br = new(fs))
             {
-                fs.Seek(0, SeekOrigin.Begin);
-                response = br.ReadInt16();
-
-                Debug.WriteLine(response);
+                try
+                {
+                    response = br.ReadInt32();
+                    Debug.WriteLine(response);
+                }
+                catch (IOException)
+                {
+                    return -1;
+                }
 
             }
+            return 0;
         }
 
         private void Init()
@@ -102,46 +109,127 @@ namespace _04_exercise
             Console.WriteLine($"Server connected on port: {ie.Port}");
             ReadNames(PATH_NAMES);
 
-            clientSocket = serverSocket.Accept();
+            while (true)
+            {
+                clientSocket = serverSocket.Accept();
 
-            new Thread(UserManagement).Start(clientSocket);
+                new Thread(UserManagement).Start(clientSocket);
+            }
 
-
-           
 
         }
 
         private void UserManagement(object clientSocket)
         {
+            bool isUserConnected = true;
+            bool isUserLogged = false;
             using (NetworkStream ns = new((Socket)clientSocket))
             using (StreamReader sr = new(ns))
             using (StreamWriter sw = new(ns))
             {
+                while (isUserConnected)
+                {
+                    if (!isUserLogged)
+                    {
+                        sw.WriteLine("WELCOME");
+                        sw.Flush();
+                        sw.WriteLine("Insert your username");
+                        sw.Flush();
+
+                        switch (GetRegisterUsername(sr, out string message))
+                        {
+
+                            case -1:
+                                isUserConnected = false;
+                                sw.WriteLine("Unknown user");
+                                break;
+                            case 0:
+                                isUserLogged = true;
+                                break;
+                            case 1:
+                                isUserLogged = true;
+                                break;
+                        }
+                    }
+                    string response = sr.ReadLine();
+
+
+                    switch (Enum.Parse(typeof(eCommands), response.ToLower()))
+                    {
+                        case eCommands.LIST:
+                            waitQueue.ForEach((x) =>
+                            {
+                                try
+                                {
+                                    sw.WriteLine(x);
+                                }
+                                catch (IOException)
+                                {
+                                    Debug.WriteLine()
+                                }
+                            });
+                            break;
+                        case eCommands.ADD:
+                            break;
+                        case eCommands.DEL:
+                            break;
+                        case eCommands.CHPIN:
+                            break;
+                        case eCommands.EXIT:
+                            break;
+                        case eCommands.SHUTDOWN:
+                            break;
+                        default:
+                            break;
+                    }
 
 
 
-                sw.WriteLine("WELCOME");
-                sw.Flush();
-                sw.WriteLine("Insert your username");
-                sw.Flush();
 
 
-
-
-
-
-
-
-
-
-
-
+                }
             }
         }
 
+        private int GetRegisterUsername(StreamReader sr, out string username)
+        {
+            string name;
 
+            try
+            {
+                name = sr.ReadLine();
+                Debug.WriteLine($"{nameof(GetRegisterUsername)} username is: {name}");
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"{nameof(GetRegisterUsername)} error");
+                username = "";
+                return -1;
+            }
 
+            if (String.IsNullOrEmpty(name))
+            {
+                username = "";
+                return -1;
+            }
+
+            if (Array.Exists(users, (user) => user == name))
+            {
+                username = name;
+                return 0;
+            }
+            else if (name.ToLower() == "admin")
+            {
+                username = name;
+                return 1;
+            }
+
+            username = "";
+            return -1;
+        }
 
     }
+
+
 
 }
