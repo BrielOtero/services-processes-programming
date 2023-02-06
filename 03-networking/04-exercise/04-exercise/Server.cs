2ad05e57f88c8d1a6c1589b1b333121f231dab02
SrMazas <info.otero.gabriel@gmail.com>
2023-02-06 14:00:13 +0100

    save

--- 03-networking/04-exercise/04-exercise/Server.cs
+++ 03-networking/04-exercise/04-exercise/Server.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,11 +16,18 @@ namespace _04_exercise
     internal class Server
     {
         private string[] users;
-        private List<string> waitQueue = new();
+        private List<User> connectedUsers = new();
+        private List<UserQueue> waitQueue = new();
 
         private const int PRIMARY_PORT = 31416;
         private const int SECONDARY_PORT = 1024;
         private readonly string PATH_NAMES = Path.Combine(Environment.GetEnvironmentVariable("userprofile"), "usuarios.txt");
+        private readonly string PATH_PIN = Path.Combine(Environment.GetEnvironmentVariable("userprofile"), "pin.bin");
+
+        private bool executeServer = true;
+        static readonly private object l = new object();
+        static readonly private object n = new object();
+
 
         private Socket serverSocket;
         private Socket clientSocket;
@@ -51,7 +59,14 @@ namespace _04_exercise
 
         private struct UserQueue
         {
-            public string Name
+            public string Name;
+            public string Time;
+
+            public UserQueue(string name, string time)
+            {
+                this.Name = name;
+                this.Time = time;
+            }
         }
 
         private bool ReadNames(string path)
@@ -73,31 +88,66 @@ namespace _04_exercise
             return true;
         }
 
-        private int ReadPin(string path)
+        private int ReadPin()
         {
-            int response;
+            int response = 1234;
 
-            using (FileStream fs = new(path, FileMode.Open))
-            using (BinaryReader br = new(fs))
+            try
             {
-                try
+                using (FileStream fs = new(PATH_PIN, FileMode.Open))
+                using (BinaryReader br = new(fs))
                 {
-                    response = br.ReadInt32();
-                    Debug.WriteLine(response);
+                    try
+                    {
+                        response = br.ReadInt32();
+                        Debug.WriteLine(response);
+                    }
+                    catch (IOException)
+                    {
+                        Debug.WriteLine($"Error in {nameof(ReadPin)} in read");
+                        return response;
+                    }
                 }
-                catch (IOException)
-                {
-                    return -1;
-                }
+                return response;
             }
-            return 0;
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error in {nameof(ReadPin)} on using");
+                return response;
+            }
+        }
+
+        private bool SavePin(uint pin)
+        {
+            try
+            {
+                using (FileStream fs = new(PATH_PIN, FileMode.OpenOrCreate))
+                using (BinaryWriter bw = new(fs))
+                {
+                    try
+                    {
+                        bw.Write(pin);
+                    }
+                    catch (IOException)
+                    {
+                        Debug.WriteLine($"Error in {nameof(ReadPin)} in write");
+                        return false;
+                    }
+                }
+                return true;
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error in {nameof(ReadPin)} on using");
+                return false;
+            }
         }
 
         public void Init()
         {
             IPEndPoint ie = new(IPAddress.Any, PRIMARY_PORT);
 
-            Socket serverSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
             {
@@ -133,38 +183,45 @@ namespace _04_exercise
 
             Console.WriteLine($"Server connected on port: {ie.Port}");
 
-
             if (!ReadNames(PATH_NAMES))
             {
                 Console.WriteLine("Error reading USERS file");
                 return;
             }
             serverSocket.Listen(18);
-            WaitForClientConnection(serverSocket);
+            WaitForClientConnection();
 
         }
 
 
-
-        private void WaitForClientConnection(Socket serverSocket)
+        private void WaitForClientConnection()
         {
-            while (true)
+            while (executeServer)
             {
-                clientSocket = serverSocket?.Accept();
-
-                new Thread(UserManagement).Start(clientSocket);
+                try
+                {
+                    clientSocket = serverSocket?.Accept();
+                    new Thread(UserManagement).Start(clientSocket);
+                }
+                catch (SocketException)
+                {
+                    Debug.WriteLine($"{nameof(WaitForClientConnection)} Server accept socket");
+                }
             }
+            Debug.WriteLine("SERVER SOCKET CLOSE");
         }
 
-       
+
 
         private void UserManagement(object userSocket)
         {
-            //bool isUserConnected = true;
-            //bool isUserLogged = false;
-            //bool isAdmin = false;
 
             User user = new((Socket)userSocket);
+
+            lock (n)
+            {
+                connectedUsers.Add(user);
+            }
 
             using (NetworkStream ns = new((Socket)user.UserSocket))
             using (StreamReader sr = new(ns))
@@ -174,40 +231,70 @@ namespace _04_exercise
 
                 while (user.IsAdmin)
                 {
-                    Debug.WriteLine("New user connected");
 
-                    user.IsAdmin = false;
+                    lock (n)
+                    {
+                        if (!executeServer) break;
+                    }
 
                     if (!user.IsLogged)
                     {
-                        sw.WriteLine("WELCOME");
-                        sw.Flush();
-                        sw.WriteLine("Insert your username");
-                        sw.Flush();
+                        user.IsAdmin = false;
+
+                        SendMessage("WELCOME", sw);
+                        SendMessage("Insert your username", sw);
 
                         switch (GetRegisterUsername(sr, out string username))
                         {
 
                             case -1:
                                 user.IsConnected = false;
-                                sw.WriteLine("Unknown user");
-                                sw.Flush();
+                                SendMessage("Unknown user", sw);
                                 break;
                             case 0:
                                 user.IsLogged = true;
                                 user.Username = username;
-                                sw.WriteLine("OK");
-                                sw.Flush();
+                                SendMessage("OK", sw);
                                 break;
                             case 1:
-                                user.IsLogged = true;
+                                SendMessage("Insert pin:", sw);
+                                int pin = ReadPin();
+
+                                if (GetMessage(out string pinString, sr) && int.TryParse(pinString, out int userPin) && pin == userPin)
+                                {
+                                    user.IsLogged = true;
+                                    user.Username = username;
+                                    user.IsAdmin = true;
+                                    SendMessage("OK", sw);
+                                }
+                                else
+                                {
+                                    SendMessage("Wrong pin", sw);
+                                    user.IsConnected = false;
+                                }
                                 break;
                         }
                     }
 
                     if (user.IsConnected)
                     {
-                        string response = sr.ReadLine();
+                        if (!GetMessage(out string response, sr))
+                        {
+                            return;
+                        }
+                        string[] responseSplit;
+
+                        try
+                        {
+                            responseSplit = response.Split(" ");
+                        }
+                        catch (Exception)
+                        {
+                            Debug.WriteLine("Split error");
+                            return;
+                        }
+
+                        response = responseSplit[0];
 
                         if (!String.IsNullOrEmpty(response))
                         {
@@ -216,43 +303,191 @@ namespace _04_exercise
                                 switch (command)
                                 {
                                     case eCommands.LIST:
-                                        waitQueue.ForEach((x) =>
-                                        {
-                                            try
-                                            {
-                                                sw.WriteLine(x);
-                                                sw.Flush();
-                                            }
-                                            catch (IOException)
-                                            {
-                                                Debug.WriteLine("Error showing waitQueue users");
-                                            }
-                                        });
+                                        Debug.WriteLine($"{eCommands.LIST.ToString()}");
 
+                                        CommandList(sw);
                                         break;
                                     case eCommands.ADD:
-                                        waitQueue.Add($"{user.Username} {DateTime.Now}");
+                                        Debug.WriteLine($"{eCommands.ADD.ToString()}");
+
+                                        CommandAdd(user, sw);
+
                                         break;
                                     case eCommands.DEL:
+                                        Debug.WriteLine($"{eCommands.DEL.ToString()}");
+
+                                        if (!user.IsAdmin) return;
+
+                                        CommandDel(sw, responseSplit);
+
                                         break;
                                     case eCommands.CHPIN:
+                                        Debug.WriteLine($"{eCommands.CHPIN.ToString()}");
+
+                                        if (!user.IsAdmin) return;
+
+                                        CommandChpin(sw, responseSplit);
+
                                         break;
                                     case eCommands.EXIT:
+                                        Debug.WriteLine($"{eCommands.EXIT.ToString()}");
+
+                                        if (!user.IsAdmin) return;
+
+                                        user.IsAdmin = false;
+
                                         break;
                                     case eCommands.SHUTDOWN:
+                                        Debug.WriteLine($"{eCommands.SHUTDOWN.ToString()}");
+
+                                        if (!user.IsAdmin) return;
+
+                                        CommandShutdown(user);
                                         break;
                                     default:
                                         break;
                                 }
                             }
-
                         }
-
                     }
+                }
 
+                lock (n)
+                {
+                    connectedUsers.Remove(user);
                 }
                 user.UserSocket.Close();
             }
+        }
+
+        private void CommandList(StreamWriter sw)
+        {
+            lock (l)
+            {
+                waitQueue.ForEach((userQueue) =>
+                {
+                    SendMessage($"{userQueue.Name} {userQueue.Time}", sw);
+                });
+            }
+        }
+
+        private void CommandAdd(User user, StreamWriter sw)
+        {
+            lock (l)
+            {
+
+                if (!Array.Exists(waitQueue.ToArray(), (queueUser) => queueUser.Name == user.Username))
+                {
+                    waitQueue.Add(new UserQueue(user.Username, DateTime.Now.ToString()));
+                    SendMessage("OK", sw);
+                }
+                else
+                {
+                    SendMessage("ERROR USER TAKEN", sw);
+                }
+
+            }
+        }
+
+
+
+        private void CommandChpin(StreamWriter sw, string[] responseSplit)
+        {
+            if (responseSplit.Length == 2)
+            {
+                lock (n)
+                {
+
+                    Debug.WriteLine($"Command {responseSplit[0]} | Pin {responseSplit[1]}");
+
+                    if (uint.TryParse(responseSplit[1], out uint pin) && pin >= 1000 && SavePin(pin))
+                    {
+                        SendMessage("OK", sw);
+                    }
+                    else
+                    {
+                        SendMessage("PIN ERROR", sw);
+                    }
+
+                    Debug.WriteLine($"Pin {pin}");
+                }
+            }
+            else
+            {
+                SendMessage("PIN ERROR", sw);
+
+            }
+        }
+
+        private void CommandDel(StreamWriter sw, string[] responseSplit)
+        {
+            if (responseSplit.Length == 2)
+            {
+                lock (l)
+                {
+
+                    Debug.WriteLine($"Command {responseSplit[0]} | Pos {responseSplit[1]} | waitQueue {waitQueue.Count}");
+
+                    if (uint.TryParse(responseSplit[1], out uint pos) && pos < waitQueue.Count)
+                    {
+                        waitQueue.RemoveAt((int)pos);
+                        SendMessage("OK", sw);
+                    }
+                    else
+                    {
+                        SendMessage("DELETE ERROR", sw);
+                    }
+
+                    Debug.WriteLine($"Pos {pos}");
+                }
+            }
+            else
+            {
+                SendMessage("DELETE ERROR", sw);
+            }
+        }
+
+        private void CommandShutdown(User user)
+        {
+            lock (n)
+            {
+                executeServer = false;
+                connectedUsers.ForEach(connectedUser =>
+                {
+                    if (!connectedUser.Equals(user))
+                    {
+                        connectedUser.UserSocket.Close();
+                    }
+                });
+                serverSocket.Close();
+            }
+        }
+
+        private void SendMessage(string message, StreamWriter sw)
+        {
+            try
+            {
+                sw.WriteLine(message);
+                sw.Flush();
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error on {nameof(SendMessage)} method sending message: {message}");
+            }
+        }
+
+        private bool GetMessage(out string message, StreamReader sr)
+        {
+            try
+            {
+                message = sr.ReadLine();
+            }
+            catch (Exception)
+            {
+                message = "";
+                return false;
+            }
+            return true;
         }
 
         private int GetRegisterUsername(StreamReader sr, out string username)
@@ -291,9 +526,5 @@ namespace _04_exercise
             username = "";
             return -1;
         }
-
     }
-
-
-
 }
