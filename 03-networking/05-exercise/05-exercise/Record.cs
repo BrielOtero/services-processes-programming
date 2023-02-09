7d028af1d68f5e998ec853bbbaa7104daf43aa5a
SrMazas <info.otero.gabriel@gmail.com>
2023-02-09 13:58:07 +0100

    save

---
+++ 03-networking/05-exercise/05-exercise/Record.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_exercise
+{
+    internal class Record : IComparable
+    {
+        private string name;
+
+        public string Name
+        {
+            set
+            {
+                if (value.Length > 3) { throw new ArgumentException("value"); }
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Seconds { set; get; }
+
+        public Record(string name, int seconds)
+        {
+            this.Name = name;
+            this.Seconds = seconds;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is null) return 1;
+
+            return this.Seconds.CompareTo((obj as Record).Seconds);
+
+        }
+    }
+
+}
--- 03-networking/05-exercise/05-exercise/Server.cs
+++ 03-networking/05-exercise/05-exercise/Server.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace _05_exercise
@@ -14,13 +16,14 @@ namespace _05_exercise
         private readonly string PATH_WORD = Path.Combine(Environment.GetEnvironmentVariable("appdata"), "words.txt");
         private readonly string PATH_RECORDS = Path.Combine(Environment.GetEnvironmentVariable("appdata"), "records.bin");
         private List<String> words;
+        private List<Record> records;
         private const int PORT = 31416;
         private bool executeServer = true;
         private Socket serverSocket;
         private Socket clientSocket;
         private enum eCommands
         {
-            GETWORD, SENDWORD, GETRECORDS, SENRECORD, CLOSESERVER
+            GETWORD, SENDWORD, GETRECORDS, SENDRECORD, CLOSESERVER
         }
 
         private bool ReadWords()
@@ -33,6 +36,71 @@ namespace _05_exercise
             catch (IOException)
             {
                 Debug.WriteLine($"Error on {nameof(ReadWords)} reading words");
+                words = new List<string>();
+                return false;
+            }
+            return true;
+        }
+
+        private bool SaveWords()
+        {
+            try
+            {
+                string fileContent = "";
+                words.ForEach(word => fileContent += word + ",");
+                File.WriteAllText(PATH_WORD, fileContent);
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error on {nameof(SaveWords)} saving words");
+                return false;
+            }
+            return true;
+        }
+
+        private bool ReadRecords()
+        {
+
+            try
+            {
+                string fileContent = Encoding.UTF8.GetString(File.ReadAllBytes(PATH_RECORDS));
+
+                List<Record>? tempRecords = JsonSerializer.Deserialize<List<Record>>(fileContent);
+
+                if (tempRecords != null)
+                {
+                    records = tempRecords;
+                }
+                else
+                {
+                    Debug.WriteLine($"Error on {nameof(SaveRecords)} null...");
+                    return false;
+                }
+
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error on {nameof(SaveRecords)} reading...");
+                records = new List<Record>()
+                {
+                    new Record("abc", 8)
+                };
+                return false;
+            }
+            return true;
+        }
+
+        private bool SaveRecords()
+        {
+
+            try
+            {
+                byte[] fileContentBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(records));
+                File.WriteAllBytes(PATH_RECORDS, fileContentBytes);
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error on {nameof(SaveRecords)} saving...");
                 return false;
             }
             return true;
@@ -61,6 +129,8 @@ namespace _05_exercise
 
             serverSocket.Listen(20);
 
+            ReadWords();
+            ReadRecords();
             WaitForUserConnection();
         }
 
@@ -103,30 +173,127 @@ namespace _05_exercise
             }
         }
 
-        private struct User
+        private void UserManagement(object userSocket)
         {
-            public Socket UserSocket;
-            public bool IsConnected;
 
-            User(Socket userSocket) : this(userSocket, true)
+            using (NetworkStream ns = new((Socket)userSocket))
+            using (StreamReader sr = new(ns))
+            using (StreamWriter sw = new(ns))
             {
-            }
+                sw.WriteLine("");
+                sw.Flush();
 
-            User(Socket userSocket, bool isConnected)
-            {
-                this.UserSocket = userSocket;
-                this.IsConnected = isConnected;
+
+                if (TryGetMessage(out string response, sr))
+                {
+
+                    string[] responseSplit = response.Split(" ");
+                    response = responseSplit[0].ToUpper();
+                    Debug.WriteLine("Command: " + response);
+
+                    if (Enum.TryParse(typeof(eCommands), response, out object command))
+                    {
+                        switch (command)
+                        {
+                            case eCommands.GETWORD:
+                                TrySendMessage(words.OrderBy(_ => new Random().Next()).ToList()[0], sw);
+                                break;
+                            case eCommands.SENDWORD:
+                                if (responseSplit.Length == 2)
+                                {
+                                    bool isCorrectlyAdded = false;
+                                    if (!words.Contains(responseSplit[1]))
+                                    {
+                                        words.Add(responseSplit[1]);
+                                        isCorrectlyAdded = SaveWords();
+                                    }
+
+                                    TrySendMessage(isCorrectlyAdded ? "OK" : "ERROR", sw);
+                                }
+                                break;
+                            case eCommands.GETRECORDS:
+
+                                if (records.Count > 0)
+                                {
+                                    string msgRecords = JsonSerializer.Serialize(records);
+                                    Debug.WriteLine("Records: " + msgRecords);
+                                    TrySendMessage(msgRecords, sw);
+                                }
+
+
+                                break;
+                            case eCommands.SENDRECORD:
+                                if (responseSplit.Length == 2)
+                                {
+
+                                    Debug.WriteLine(nameof(eCommands.SENDRECORD));
+
+                                    bool isCorrectlyAdded = false;
+
+                                    Record? newRecord = JsonSerializer.Deserialize<Record>(responseSplit[1]);
+
+                                    if (newRecord != null)
+                                    {
+                                        if (records.Count == 3)
+                                        {
+                                            Record maxRecord = records.Max();
+
+                                            if (newRecord.Seconds < maxRecord.Seconds)
+                                            {
+                                                records.Remove(maxRecord);
+                                                records.Add(newRecord);
+                                                isCorrectlyAdded = SaveRecords();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            records.Add(newRecord);
+                                            isCorrectlyAdded = SaveRecords();
+                                        }
+                                    }
+
+                                    TrySendMessage(isCorrectlyAdded ? "ACCEPT" : "REJECT", sw);
+                                }
+                                break;
+                            case eCommands.CLOSESERVER:
+                                break;
+                        }
+                    }
+                }
             }
+            (userSocket as Socket).Close();
 
         }
 
-        private void UserManagement(Object userSocket)
+        private bool TryGetMessage(out string response, StreamReader sr)
         {
-
+            try
+            {
+                response = sr.ReadLine();
+                return true;
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine("Error getting message");
+                response = "";
+                return false;
+            }
         }
 
-
-
+        private bool TrySendMessage(string message, StreamWriter sw)
+        {
+            try
+            {
+                sw.WriteLine(message);
+                sw.Flush();
+            }
+            catch (IOException)
+            {
+                Debug.WriteLine($"Error on {nameof(TrySendMessage)} sending message");
+                return false;
+            }
+            return true;
+        }
 
     }
 
